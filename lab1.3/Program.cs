caf0402329d7eb45f687517b2fb903115f19a21f
SlesarenkoY <67012914+SlesarenkoY@users.noreply.github.com>
2020-10-05 22:30:24 +0700

    lab1.3

---
+++ lab1.3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Fib
+{
+    class Program
+    {
+        static int curV = 1;
+        static int lastV = 1;
+        static int numFib = 0;
+        public static void number(uint n)
+        {
+            Console.WriteLine(curV);
+            curV = lastV + numFib;
+            numFib = lastV;
+            lastV = curV;
+            if (curV < n)
+                number(n);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Максимальный значение: ");
+            try
+            {
+                if (!uint.TryParse(Console.ReadLine(), out uint g)) throw new ArithmeticException();
+                else number(g);
+            }
+            catch (ArithmeticException)
+            {
+                Console.WriteLine("Введите число или цифру!");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Что то пошло не так, введите другое значение!");
+            }
+            Console.Read();
+        }
+    }
+}
